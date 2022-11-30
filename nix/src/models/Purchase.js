ae9e7db751403112f2195092abbdc127b914ebcf
fabiocaettano <fabio.caettano74@gmail.com>
2022-11-30 21:53:06 +0000

    implantado model Purchase

--- nix/src/models/Purchase.js
+++ nix/src/models/Purchase.js
@@ -1,29 +1,9 @@
 const mongoose = require('mongoose');
 
 const PurchaseSchema = new mongoose.Schema({
-    userId: Strint,
+    userId: String,
     title: String,
     value: Number,    
 });
-
-UserSchema.pre('save', async function(next){
-    if (!this.isModified('password')) next();
-
-    this.password = await bcrypt.hash(this.password, 8);
-});
-
-UserSchema.methods = {
-    compareHash(hash){
-        return bcrypt.compare(hash, this.password);
-    },
-};
-
-UserSchema.statics = {
-    generateToken({ id }) {
-        return jwt.sign( { id }, 'michele', {
-           expiresIn: 86400, 
-        });
-    },
-};
 
 module.exports = mongoose.model('Purchase', PurchaseSchema);
