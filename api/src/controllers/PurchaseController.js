83a8a8ed6e69535cd99ce0933aac770ff6e4ae15
fabiocaettano <fabio.caettano74@gmail.com>
2022-11-30 22:14:25 +0000

    implementado os metodos index, show e store

--- api/src/controllers/PurchaseController.js
+++ api/src/controllers/PurchaseController.js
@@ -1,16 +1,57 @@
-class PurchaseController {
-    async index(req, res){
-        const { id } = req.params;
+const NixService = require('../services/nix');
 
+class PurchaseController {
+    
+    async index(req, res) {
+        
+        const response =  await new Promise((resolve, reject) => {
+
+            NixService.listPurchases({ userId: req.userId }, (err, response) => {
+                if (err) {
+                    reject(err);
+                } else {
+                    resolve(response);
+                }
+            });
+        });
+
+        return res.json(response);
     }
 
     async show(req, res){  
+
+        const { id } = req.params;
+
+        const response = await new Promise((resolve, reject) => {
+
+            NixService.getPurchaseById({ id }, (err, response) => {
+                if ( err ) {
+                    reject(err);
+                } else {
+                    resolve(response);
+                }
+            });
+        });
+    
+        return res.json(response);
     }
     
     async store(req,res){
-        const { userId } = req;
-     
+        
+        const { title, value } = req.body;
 
+        const response = await new Promise((resolve, reject) => {
+
+            NixService.purchase({title, value, userId: req.userId },(err, response) => {
+                if (err ) {
+                    reject(err);
+                } else {
+                    resolve(response);
+                }
+            });
+        });
+     
+        return res.json(response);
     }
 }
 
