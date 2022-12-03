0166150dc61646cb1b39fc87b8c472f9678858eb
fabiocaettano <fabio.caettano74@gmail.com>
2022-12-03 22:26:48 +0000

    excluido diretório hidra e nix

--- nix/src/database/index.js
+++
@@ -1,7 +0,0 @@
-const mongoose = require('mongoose');
-
-//iniciar conexão com o banco de dados
-mongoose.connect('mongodb://db_purchase:27018/nix',{
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-});
