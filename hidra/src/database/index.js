47633bd6f39bc93d229ef7a7a4b48ef5650a3d8f
fabiocaettano <fabio.caettano74@gmail.com>
2022-12-03 22:24:53 +0000

    excluido diretório hidra

--- hidra/src/database/index.js
+++
@@ -1,7 +0,0 @@
-const mongoose = require('mongoose');
-
-//iniciar conexão com o banco de dados
-mongoose.connect('mongodb://db-hidra:27017/hydra',{
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-});
