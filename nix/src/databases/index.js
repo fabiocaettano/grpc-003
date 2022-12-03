14f7c7368c109a6ce29b33d7c759f5c5ab899d6b
fabiocaettano <fabio.caettano74@gmail.com>
2022-12-03 22:01:47 +0000

    excluido diretório nix

--- nix/src/databases/index.js
+++
@@ -1,7 +0,0 @@
-const mongoose = require('mongoose');
-
-//iniciar conexão com o banco de dados
-mongoose.connect('mongodb://db:27017/nix',{
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-});
